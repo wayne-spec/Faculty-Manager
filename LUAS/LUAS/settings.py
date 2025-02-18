6da4e6542fb15198c2a17232c892a7f119373b5e
wayne-spec <wayne.opanja@strathmore.edu>
2025-02-18 23:27:06 +0300

    CORS Allowing requests from the front-end.

--- LUAS/LUAS/settings.py
+++ LUAS/LUAS/settings.py
@@ -167,5 +167,5 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',  # React frontend
+    'https://faculty-manager-front-end-g6m8.vercel.app',  # React frontend
 ]
